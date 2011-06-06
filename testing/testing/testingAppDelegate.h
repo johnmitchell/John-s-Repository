4fd9d9c10c436786ff0c8911797b3b97b1f6b87b
John Mitchell <john.mitchell@newsint.co.uk>
2011-06-06 14:59:56 +0100

    Changed created name.

--- testing/testing/testingAppDelegate.h
+++ testing/testing/testingAppDelegate.h
@@ -2,7 +2,7 @@
 //  testingAppDelegate.h
 //  testing
 //
-//  Created by newsint on 06/06/2011.
+//  Created by johnmitchell on 06/06/2011.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
