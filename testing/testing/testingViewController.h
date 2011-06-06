b83de80d58e26f0e011d0ad3de371b19a02dd1ac
Abid Ghani <abid.ghani@newsint.co.uk>
2011-06-06 14:33:09 +0100

    bloody commit

---
+++ testing/testing/testingViewController.h
@@ -0,0 +1,15 @@
+//
+//  testingViewController.h
+//  testing
+//
+//  Created by newsint on 06/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testingViewController : UIViewController {
+    
+}
+
+@end
